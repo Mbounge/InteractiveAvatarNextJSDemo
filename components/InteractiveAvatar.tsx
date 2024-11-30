c2b31f10af693b4b9cfe39d5d17bddce7d9e4819
Mbounge <mndlovu76@yahoo.com>
2024-11-29 21:50:46 -0400

    set avatar quality to high

--- components/InteractiveAvatar.tsx
+++ components/InteractiveAvatar.tsx
@@ -737,12 +737,12 @@ export default function InteractiveAvatar() {
 
     try {
       const res = await avatar.current.createStartAvatar({
-        quality: AvatarQuality.Low,
+        quality: AvatarQuality.High,
         avatarName: avatarId,
         //knowledgeId: "", // Or use a custom `knowledgeBase`.
         knowledgeBase: custom,
         voice: {
-          rate: 1.5, // 0.5 ~ 1.5
+          rate: 1.0, // 0.5 ~ 1.5
           emotion: VoiceEmotion.EXCITED,
         },
         language: language,
