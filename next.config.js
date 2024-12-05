72cc26e7dbb973c9abb156efa5bdb767053abfc6
Mbounge <mndlovu76@yahoo.com>
2024-12-05 18:35:18 -0400

    tailwind and next config fixes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
-    experimental: {
-      appDir: true,
-      serverActions: true
-      
-    },
     eslint: {
         ignoreDuringBuilds: true, // Ignore ESLint for production builds
       },
