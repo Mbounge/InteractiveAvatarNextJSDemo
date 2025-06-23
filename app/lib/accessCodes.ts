35adafdf6d928e4a3a676aa1e235232319a32643
Mbounge <mndlovu76@yahoo.com>
2025-06-23 13:13:52 -0300

    accessCode update

--- app/lib/accessCodes.ts
+++ app/lib/accessCodes.ts
@@ -1,9 +1,9 @@
 const validCodes = new Set([
-    "GRT48S2A",
-    "SC9T3B7K",
-    "RPT2C1V8",
-    "PLR6D9M5",
-    "AGNT4F3Z",
+    "IvanGRT48S2A",
+    "KroniSC9T3B7K",
+    "DanRPT2C1V8",
+    "Tbd1PLR6D9M5",
+    "Tbd2AGNT4F3Z",
   ]);
   
   export function isValidAccessCode(code: string): boolean {
